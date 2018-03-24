5170d226fd502dde0a1ca353f64f735b7841cfc6
C. Annable <christopher.annable@gmail.com>
2018-03-23 22:03:48 -0400

    Update micro builds to create user at container start.

--- 9.0-micro/data/init.sh
+++ 9.0-micro/data/init.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
+# ------------------------------------------------------------------------------
+# Tomcat Init Script
+# ------------------------------------------------------------------------------
+
+# Set up user
+echo Setting up user...
+addgroup -g $TC_UID $TC_USER
+adduser -u $TC_UID -G $TC_USER -S -s /usr/sbin/nologin $TC_USER
+chown -R $TC_USER:$TC_USER $CATALINA_HOME
+#chgrp -R $TC_USER $CATALINA_HOME
+
+# Start Tomcat
+echo Starting Tomcat...
 su -p -l -s /bin/sh -c "/bin/sh $CATALINA_HOME/bin/catalina.sh run" $TC_USER
