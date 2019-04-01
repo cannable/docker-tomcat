f65bce2a34be9eb5c29380beb9f6ff3e5b56ca8e
C. Annable <christopher.annable@gmail.com>
2019-03-31 20:33:04 -0400

    Add LOG4J2_VERSION environment variable to the prototype log4j2 Dockerfile to control the version that gets installed. Enhance log4j2.sh to write more helpful comments to stdout during the container build process.

--- log4j2/log4j2.sh
+++ log4j2/log4j2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ver=2.11.2
+ver=$LOG4J2_VERSION
 filename=apache-log4j-${ver}-bin.tar.gz
 dirname=apache-log4j-${ver}-bin
 url_keys=https://www.apache.org/dist/logging/KEYS
@@ -9,21 +9,24 @@ url_sig=https://www.apache.org/dist/logging/log4j/${ver}
 destdir="$CATALINA_HOME/log4j2"
 
 
+echo "log4j2 version: $ver"
 cd /data/log4j2/
 
 apk add --no-cache gnupg
 
 # Grab the log4j PGP keys
+echo Importing the Apache Logging PGP KEYS...
 wget -O - ${url_keys} | gpg --import -
 
 
 # Get the bits
+echo Downloading tarball and signature...
 wget "${url_bin}/${filename}"
 wget "${url_sig}/${filename}.asc"
 
 
 # Check the download signature
-#check=$(gpg --status-fd 1 --verify "${filename}.asc")
+echo Checking tarball signature...
 gpg --verify "${filename}.asc"
 
 if [[ $? -ne 0 ]]; then
@@ -38,24 +41,32 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
+
 echo Tarball passed PGP signature check.
 
 # Extraction of the files is indirect, as we're working around limitations in
 # busybox's implementation of tar
 
+
+echo Extracting select files from tarball...
 tar -xf "${filename}" -C . \
     "${dirname}/log4j-api-${ver}.jar" \
     "${dirname}/log4j-core-${ver}.jar" \
     "${dirname}/log4j-appserver-${ver}.jar"
 
+
+echo Creating log4j2 directory structure...
 mkdir -p "$destdir/lib" "$destdir/conf"
 
+
+echo Installing log4j2 jars...
 cp \
     "${dirname}/log4j-api-${ver}.jar" \
     "${dirname}/log4j-core-${ver}.jar" \
     "${dirname}/log4j-appserver-${ver}.jar" \
     "$destdir/lib"
 
+echo Applying log4j2 configuration diversions...
 cp setenv.sh "$CATALINA_HOME/bin"
 cp log4j2-tomcat.xml "$CATALINA_HOME/conf"
 ln -s "$CATALINA_HOME/conf/log4j2-tomcat.xml" "$destdir/conf/log4j2-tomcat.xml"
@@ -63,7 +74,7 @@ rm "$CATALINA_HOME/conf/logging.properties"
 
 
 # Cleanup
-
+echo Cleaning up staging area...
 cd /data
 rm -rf ./log4j2
 
