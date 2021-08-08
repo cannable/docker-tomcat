63229f12629efeed058a2e29d1f266faf22d0b60
C. Annable <christopher.annable@gmail.com>
2021-08-08 15:47:21 -0400

    Install log4j2.

--- util/buildah_all.sh
+++ util/buildah_all.sh
@@ -5,6 +5,8 @@
 
 IMAGE="cannable/tomcat"
 JDK_MAJOR_VERSION=11
+LOG4J2_ENABLED=1
+LOG4J2_VERSION=2.14.1
 #ARCHES=(amd64 arm64)
 ARCHES=(amd64)
 #TC_VERSIONS=(8.5.69 9.0.52 10.0.10)
@@ -24,7 +26,9 @@ cache() {
     cachename="cache/${fname}"
 
     # Only download file if we need to
-    if [ ! -f "${cachename}" ]; then
+    if [ -f "${cachename}" ]; then
+        echo "${fname} is already cached. Not downloading."
+    else
         echo "Caching ${fname} (${url})..."
         curl -o "${cachename}" "${url}"
     fi
@@ -76,15 +80,11 @@ build_image() {
 
     # Copy over the artefacts
     buildah copy $c "cache/${filename}" /tmp
-    buildah copy $c "cache/${filename}.asc" /tmp
 
     buildah run $c tar -xzf "/tmp/${filename}" -C /tmp/
     buildah run $c mv "/tmp/${pkgname}/" "${CATALINA_HOME}"
 
-    buildah run $c rm -f \
-        "/tmp/${filename}" \
-        "/tmp/${filename}.asc"
-
+    buildah run $c rm -f "/tmp/${filename}"
 
     # Prune default webapps
     echo Pruning default webapps...
@@ -97,10 +97,7 @@ build_image() {
     buildah run $c mkdir /data
 
     # setenv.sh redirection
-    buildah copy \
-        --chown root:root \
-        --chmod 0644 \
-        $c data/setenv.sh "${CATALINA_HOME}/conf/setenv.sh"
+    buildah copy $c data/setenv.sh "${CATALINA_HOME}/conf/setenv.sh"
 
     buildah run $c ln -s "${CATALINA_HOME}/conf/setenv.sh" "${CATALINA_HOME}/bin/setenv.sh"
 
@@ -108,6 +105,10 @@ build_image() {
         --chown root:root \
         --chmod 0755 \
         $c data/init.sh /data/init.sh
+
+    if [ "$LOG4J2_ENABLED" -gt "0" ]; then
+        install_log4j $c $LOG4J2_VERSION
+    fi
 
     buildah config  \
         --env "NAME=${NAME}" \
@@ -129,6 +130,84 @@ build_image() {
     buildah commit --format docker --rm $c "${IMAGE}:${arch}-${ver}"
 }
 
+get_tomcat() {
+    local ver=$1
+
+    # Generate URLs
+    local major_ver=$(echo $ver | awk -F . -e '{print $1}')
+
+    local pkgname="apache-tomcat-${ver}"
+    local filename="apache-tomcat-${ver}.tar.gz"
+
+    local url="https://archive.apache.org/dist/tomcat/tomcat-${major_ver}/v${ver}/bin/${filename}"
+
+    # Get signing key(s)
+    import_pgp_keys "https://downloads.apache.org/tomcat/tomcat-${major_ver}/KEYS"
+
+    # Download tar archive and signature, if we need to
+    cache $filename $url
+    cache "${filename}.asc" "${url}.asc"
+
+    # Check signature
+    verify "${filename}.asc"
+}
+
+get_log4j() {
+    local ver=$1
+
+    local filename="apache-log4j-${ver}-bin.tar.gz"
+    local url="https://archive.apache.org/dist/logging/log4j/${ver}/${filename}"
+
+    import_pgp_keys "https://downloads.apache.org/logging/KEYS"
+
+    cache $filename $url
+    cache "${filename}.asc" "${url}.asc"
+
+    verify "${filename}.asc"
+}
+
+install_log4j() {
+    local c=$1
+    local ver=$2
+
+    local ver=2.14.1
+    local dirname=apache-log4j-${ver}-bin
+    local filename="${dirname}.tar.gz"
+
+    echo Installing Log4j ${ver}...
+
+    buildah copy $c "cache/${filename}" /tmp
+
+    buildah run $c mkdir -p \
+        "${CATALINA_HOME}/log4j2/conf" \
+        "${CATALINA_HOME}/log4j2/lib"
+
+    for jar in log4j-api log4j-core log4j-appserver; do
+
+        buildah run $c tar -xzf "/tmp/${filename}" \
+            -C "/tmp" \
+            "${dirname}/${jar}-${ver}.jar"
+
+        buildah run $c mv "/tmp/${dirname}/${jar}-${ver}.jar" "${CATALINA_HOME}/log4j2/lib"
+    done
+
+    buildah run $c rm -rf "/tmp/${filename}" "/tmp/${dirname}"
+
+    buildah copy $c "log4j2/setenv.sh" "${CATALINA_HOME}/conf"
+    buildah copy $c "log4j2/log4j2-tomcat.xml" "${CATALINA_HOME}/log4j2/conf"
+
+    buildah run $c rm "${CATALINA_HOME}/conf/logging.properties"
+
+}
+
+import_pgp_keys() {
+    local url=$1
+
+    # Import keys
+    echo Importing PGP keys from ${url}
+    curl ${url} | gpg --import -
+}
+
 
 # ------------------------------------------------------------------------------
 # Sanity Checks
@@ -144,31 +223,18 @@ fi
 
 
 for ver in ${TC_VERSIONS[@]}; do
-    # Generate URLs
-    major_ver=$(echo $ver | awk -F . -e '{print $1}')
-    url_keys="https://downloads.apache.org/tomcat/tomcat-${major_ver}/KEYS"
-
-    pkgname="apache-tomcat-${ver}"
-    filename="apache-tomcat-${ver}.tar.gz"
-
-    url_bin="https://archive.apache.org/dist/tomcat/tomcat-${major_ver}/v${ver}/bin/${filename}"
-    url_sig="${url_bin}.asc"
-
-    # Import keys
-    echo Importing the Apache Tomcat PGP KEYS...
-    curl ${url_keys} | gpg --import -
-
-    # Download tar archive and signature, if we need to
-    cache $filename $url_bin
-    cache "${filename}.asc" $url_sig
-
-    # Check signature
-    verify "${filename}.asc"
+    get_tomcat $ver
 done
 
+if [ "$LOG4J2_ENABLED" -gt "0" ]; then
+    echo Log4j is enabled for this build.
+
+    get_log4j $c $LOG4J2_VERSION
+fi
 
 # ------------------------------------------------------------------------------
 # Build images
+
 
 for arch in ${ARCHES[@]}; do
     echo Building common-base image for $arch...
